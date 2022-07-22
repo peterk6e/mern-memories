075ee1b7d26df4816b85bbe55c1fb69af5fdbee9
pierrek6e <pierrecass75@gmail.com>
2022-07-22 11:29:51 +0700

    show params in edit form

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { TextField, Button, Typography, Paper } from "@material-ui/core"
 import FileBase from "react-file-base64"
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { createPost, updatePost } from "../../actions/posts"
 
 import useStyles from "./styles"
 
 const Form = ({ currentId, setCurrentId }) => {
+  const classes = useStyles()
   const dispatch = useDispatch()
+  const post = useSelector(state =>
+    currentId ? state.posts.find(post => post._id === currentId) : null
+  )
 
   const [postData, setPostData] = useState({
     creator: "",
@@ -16,7 +20,10 @@ const Form = ({ currentId, setCurrentId }) => {
     tags: "",
     selectedFile: "",
   })
-  const classes = useStyles()
+
+  useEffect(() => {
+    if(post) setPostData(post)
+  }, [post])
 
   const handleSubmit = e => {
     e.preventDefault()
