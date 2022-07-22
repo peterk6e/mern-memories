0b8ed9173a6ddde2153d6609e57d20505b227c4e
pierrek6e <pierrecass75@gmail.com>
2022-07-22 12:03:26 +0700

    show whole error in console log

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -7,7 +7,7 @@ export const getPosts = async (req, res) => {
 
     res.status(200).json(postMessages)
   } catch (error) {
-    res.status(404).json({ message: error.message })
+    res.status(404).json({ message: error })
   }
 
   res.send("This works")
@@ -21,20 +21,24 @@ export const createPost = async (req, res) => {
     await newPost.save()
     res.status(201).json(newPost)
   } catch (error) {
-    res.status(409).json({ message: error.message })
+    res.status(409).json({ message: error })
   }
 }
 
 export const updatePost = async (req, res) => {
-  const { id: _id } = req.params // destructure and rename 'id' to match terminoly with Mongoose.
+  const { id } = req.params
   const post = req.body
 
   if (!mongoose.Types.ObjectId.isValid(_id))
     return res.status(404).send("No post with that ID")
 
-  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
-    new: true,
-  })
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    _id,
+    { ...post, _id: id },
+    {
+      new: true,
+    }
+  )
 
   res.json(updatedPost)
 }
