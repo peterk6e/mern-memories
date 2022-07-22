8be920787c7afde5afb8a63d2075ba4f89b6163c
pierrek6e <pierrecass75@gmail.com>
2022-07-22 09:48:42 +0700

    add update post function on serverside

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,25 +1,40 @@
-import PostMessage from "../models/postMessage.js";
+import mongoose from "mongoose"
+import PostMessage from "../models/postMessage.js"
 
 export const getPosts = async (req, res) => {
   try {
-    const postMessages = await PostMessage.find();
+    const postMessages = await PostMessage.find()
 
-    res.status(200).json(postMessages);
+    res.status(200).json(postMessages)
   } catch (error) {
-    res.status(404).json({ message: error.message });
+    res.status(404).json({ message: error.message })
   }
 
-  res.send("This works");
-};
+  res.send("This works")
+}
 
 export const createPost = async (req, res) => {
-  const post = req.body;
-  const newPost = new PostMessage(post);
+  const post = req.body
+  const newPost = new PostMessage(post)
 
   try {
-    await newPost.save();
-    res.status(201).json(newPost);
+    await newPost.save()
+    res.status(201).json(newPost)
   } catch (error) {
-    res.status(409).json({ message: error.message });
+    res.status(409).json({ message: error.message })
   }
-};
+}
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params // destructure and rename 'id' to match terminoly with Mongoose.
+  const post = req.body
+
+  if (!mongoose.Types.ObjectId.isValid(_id))
+    return res.status(404).send("No post with that ID")
+
+  const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, {
+    new: true,
+  })
+
+  res.json(updatedPost)
+}
