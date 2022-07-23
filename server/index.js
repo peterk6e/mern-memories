aef3ded9ed23a963306d4e689d945d8a8d9b44a1
pierrek6e <pierrecass75@gmail.com>
2022-07-23 10:19:51 +0700

    add env variables

--- server/index.js
+++ server/index.js
@@ -1,31 +1,28 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from 'dotenv'
+import postRoutes from "./routes/posts.js"
 
-import postRoutes from './routes/posts.js'
+const app = express()
+dotenv.config()
 
-const app = express();
+app.use(bodyParser.json({ limits: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limits: "30mb", extended: true }))
+app.use(cors({ origin: "http://localhost:3000" }))
 
-
-app.use(bodyParser.json({ limits: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limits: "30mb", extended: true }));
-app.use(cors({ origin: 'http://localhost:3000' }));
-
-app.use('/posts', postRoutes); // prefix 'posts'
-// const pwd = process.env.MONGODB_PASSWORD;
-// https://www.mongodb.com/cloud/atlas
-const CONNECTION_URL = `mongodb+srv://peterk6e:peterk6e123@cluster0.tj9a1.mongodb.net/?retryWrites=true&w=majority`;
-const PORT = process.env.PORT || 5000;
+app.use("/posts", postRoutes) // prefix 'posts'
+const PORT = process.env.PORT || 5000
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() =>
     app.listen(PORT, () => console.log(`server running on port: ${PORT}`))
   )
-  .catch((error) => console.log(error.message));
+  .catch(error => console.log(error.message))
 
-  // mongoose.set('useFindAndModify', false)
+// mongoose.set('useFindAndModify', false)
