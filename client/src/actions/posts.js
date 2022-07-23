0bf291e81109757db6c779dcc30b9ddacfccf342
pierrek6e <pierrecass75@gmail.com>
2022-07-23 10:02:06 +0700

    add constants

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,11 @@
 import * as api from "../api"
+import {
+  FETCH_ALL,
+  UPDATE,
+  CREATE,
+  LIKE,
+  DELETE,
+} from "./../constants/actionTypes"
 
 // Actions Creators
 // use redux thunk
@@ -6,7 +13,7 @@ export const getPosts = () => async dispatch => {
   try {
     const { data } = await api.fetchPosts()
 
-    dispatch({ type: "FETCH_ALL", payload: data })
+    dispatch({ type: FETCH_ALL, payload: data })
   } catch (error) {
     console.log(error)
   }
@@ -16,7 +23,7 @@ export const createPost = post => async dispatch => {
   try {
     const { data } = await api.createPost(post)
 
-    dispatch({ type: "CREATE", payload: data })
+    dispatch({ type: CREATE, payload: data })
   } catch (error) {
     console.log(error)
   }
@@ -26,7 +33,7 @@ export const updatePost = (id, post) => async dispatch => {
   try {
     const { data } = await api.updatePost(id, post)
 
-    dispatch({ type: "UPDATE", payload: data })
+    dispatch({ type: UPDATE, payload: data })
   } catch (error) {
     console.log(error)
   }
@@ -36,7 +43,7 @@ export const deletePost = id => async dispatch => {
   try {
     await api.deletePost(id)
 
-    dispatch({ type: "DELETE", payload: id })
+    dispatch({ type: DELETE, payload: id })
   } catch (error) {
     console.log(error)
   }
@@ -46,7 +53,7 @@ export const likePost = id => async dispatch => {
   try {
     const { data } = await api.likePost(id)
 
-    dispatch({ type: "LIKE", payload: data })
+    dispatch({ type: LIKE, payload: data })
   } catch (error) {
     console.log(error)
   }
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,15 +1,23 @@
+import {
+  FETCH_ALL,
+  UPDATE,
+  CREATE,
+  LIKE,
+  DELETE,
+} from "./../constants/actionTypes"
+
 export default (posts = [], action) => {
   switch (action.type) {
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload]
-    case "UPDATE":
-    case "LIKE": // both cases are the same
+    case UPDATE:
+    case LIKE: // both cases are the same
       return posts.map(post =>
         action.payload._id === post._id ? action.payload : post
       )
-    case "DELETE":
+    case DELETE:
       return posts.filter(post => post._id !== action.payload)
     default:
       return posts
