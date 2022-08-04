483fa56f91aa5e6351e9165fd999b87bd9b92025
pierrek6e <pierrecass75@gmail.com>
2022-08-04 09:39:36 +0700

    fix env var

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API = axios.create({ baseURL: REACT_APP_BASE_URL })
+const API = axios.create({ baseURL: process.env.REACT_APP_BASE_URL })
 
 API.interceptors.request.use(req => {
   const profile = localStorage.getItem("profile")
